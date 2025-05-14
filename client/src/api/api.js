b1a8abd17f58f5795c2bb8989a806ed45b61a649
Deepika <deepikajaiswal305@gmail.com>
2025-05-14 23:36:34 +0530

    6th commit

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 import { api_url } from '../utils/config'
 const api = axios.create({
-     baseURL: 'http://multi-vendor-5z5y.onrender.com/api'
+     baseURL: 'https://multi-vendor-5z5y.onrender.com/api'
 })
 export default api
